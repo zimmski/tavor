fe8edabf3eb9c3d38bfdf3332b3bfc45ccaa0021
Markus Zimmermann <markus.zimmermann@nethead.at>
2014-05-31 16:06:42 +0200

    implement RandTest a random generator for testing

---
+++ test/rand.go
@@ -0,0 +1,41 @@
+package test
+
+import (
+	"math"
+)
+
+type RandTest struct {
+	seed int64
+}
+
+func NewRandTest(seed int64) *RandTest {
+	return &RandTest{
+		seed: seed,
+	}
+}
+
+func (r *RandTest) rand(n int64) int {
+	if r.seed == math.MaxInt64 {
+		r.seed = 0
+	}
+
+	if r.seed < n {
+		r.seed++
+	} else {
+		r.seed = 0
+	}
+
+	return int(r.seed)
+}
+
+func (r *RandTest) Int() int {
+	return r.rand(r.seed + 1)
+}
+
+func (r *RandTest) Intn(n int) int {
+	return r.rand(int64(n - 1))
+}
+
+func (r *RandTest) Seed(seed int64) {
+	r.seed = seed
+}
---
+++ test/rand_test.go
@@ -0,0 +1,25 @@
+package test
+
+import (
+	"testing"
+
+	. "github.com/stretchr/testify/assert"
+)
+
+func TestRandTest(t *testing.T) {
+	o := NewRandTest(0)
+
+	Equal(t, 1, o.Int())
+	Equal(t, 2, o.Int())
+	Equal(t, 3, o.Int())
+
+	o.Seed(1)
+	Equal(t, 2, o.Int())
+	Equal(t, 3, o.Int())
+
+	Equal(t, 4, o.Intn(10))
+	Equal(t, 0, o.Intn(1))
+	Equal(t, 1, o.Intn(2))
+	Equal(t, 0, o.Intn(2))
+	Equal(t, 1, o.Intn(2))
+}
