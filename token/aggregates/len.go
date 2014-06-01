7a280b0f694bbcfa055204efe63849a10624f09e
Markus Zimmermann <markus.zimmermann@nethead.at>
2014-06-01 17:45:17 +0200

    Len aggregate token

---
+++ token/aggregates/len.go
@@ -0,0 +1,33 @@
+package aggregates
+
+import (
+	"strconv"
+
+	"github.com/zimmski/tavor/rand"
+	"github.com/zimmski/tavor/token"
+	"github.com/zimmski/tavor/token/lists"
+)
+
+type Len struct {
+	list lists.List
+}
+
+func NewLen(list lists.List) *Len {
+	return &Len{
+		list: list,
+	}
+}
+
+func (a *Len) Clone() token.Token {
+	return &Len{
+		list: a.list,
+	}
+}
+
+func (a *Len) Fuzz(r rand.Rand) {
+	// do nothing
+}
+
+func (a *Len) String() string {
+	return strconv.Itoa(a.list.Len())
+}
---
+++ token/aggregates/len_test.go
@@ -0,0 +1,40 @@
+package aggregates
+
+import (
+	"testing"
+
+	. "github.com/stretchr/testify/assert"
+
+	"github.com/zimmski/tavor/test"
+	"github.com/zimmski/tavor/token"
+	"github.com/zimmski/tavor/token/lists"
+	"github.com/zimmski/tavor/token/primitives"
+)
+
+func TestLenTokensToBeTokens(t *testing.T) {
+	var tok *token.Token
+
+	Implements(t, tok, NewLen(lists.NewAll(primitives.NewConstantInt(1))))
+}
+
+func TestConstantInt(t *testing.T) {
+	list := lists.NewLeast(primitives.NewConstantInt(1), 1)
+	Equal(t, "1", list.String())
+
+	o := NewLen(list)
+	Equal(t, "1", o.String())
+
+	r := test.NewRandTest(0)
+	list.Fuzz(r)
+	Equal(t, "11", list.String())
+	Equal(t, "2", o.String())
+
+	list.Fuzz(r)
+	Equal(t, "111", list.String())
+	Equal(t, "3", o.String())
+
+	o.Fuzz(r)
+
+	o2 := o.Clone()
+	Equal(t, o.String(), o2.String())
+}
