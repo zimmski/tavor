7e522d9c1bd30d2791bef59d9d71eb7562fe753a
Markus Zimmermann <markus.zimmermann@nethead.at>
2014-06-01 12:22:18 +0200

    Most list token

---
+++ token/lists/most.go
@@ -0,0 +1,65 @@
+package logicals
+
+import (
+	"bytes"
+
+	"github.com/zimmski/tavor/rand"
+	"github.com/zimmski/tavor/token"
+)
+
+type Most struct {
+	n     int64
+	token token.Token
+	value []token.Token
+}
+
+func NewMost(tok token.Token, n int64) *Most {
+	m := &Most{
+		n:     n,
+		token: tok,
+		value: make([]token.Token, n),
+	}
+
+	for i, _ := range m.value {
+		m.value[i] = tok.Clone()
+	}
+
+	return m
+}
+
+func (m *Most) Clone() token.Token {
+	c := Most{
+		n:     m.n,
+		token: m.token,
+		value: make([]token.Token, len(m.value)),
+	}
+
+	for i, tok := range m.value {
+		c.value[i] = tok.Clone()
+	}
+
+	return &c
+}
+
+func (m *Most) Fuzz(r rand.Rand) {
+	n := r.Intn(int(m.n) + 1)
+	toks := make([]token.Token, n)
+
+	for i, _ := range toks {
+		toks[i] = m.token.Clone()
+
+		toks[i].Fuzz(r)
+	}
+
+	m.value = toks
+}
+
+func (m *Most) String() string {
+	var buffer bytes.Buffer
+
+	for _, tok := range m.value {
+		buffer.WriteString(tok.String())
+	}
+
+	return buffer.String()
+}
---
+++ token/lists/most_test.go
@@ -0,0 +1,41 @@
+package logicals
+
+import (
+	"testing"
+
+	. "github.com/stretchr/testify/assert"
+
+	"github.com/zimmski/tavor/test"
+	"github.com/zimmski/tavor/token"
+	"github.com/zimmski/tavor/token/primitives"
+)
+
+func TestMostTokensToBeTokens(t *testing.T) {
+	var tok *token.Token
+
+	a := primitives.NewRandomInt()
+
+	Implements(t, tok, NewMost(a, 1))
+}
+
+func TestMost(t *testing.T) {
+	a := primitives.NewConstantString("a")
+
+	o := NewMost(a, 5)
+	Equal(t, "aaaaa", o.String())
+
+	r := test.NewRandTest(1)
+	o.Fuzz(r)
+	Equal(t, "aa", o.String())
+
+	b := primitives.NewRangeInt(1, 3)
+	o = NewMost(b, 4)
+	Equal(t, "1111", o.String())
+
+	r.Seed(2)
+	o.Fuzz(r)
+	Equal(t, "123", o.String())
+
+	o2 := o.Clone()
+	Equal(t, o.String(), o2.String())
+}
