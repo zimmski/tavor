3b39d2a74f82a7b084d0c26caaab2e1d036514f4
Markus Zimmermann <markus.zimmermann@nethead.at>
2014-06-01 12:55:51 +0200

    Repeat list token

---
+++ token/lists/repeat.go
@@ -0,0 +1,68 @@
+package logicals
+
+import (
+	"bytes"
+
+	"github.com/zimmski/tavor/rand"
+	"github.com/zimmski/tavor/token"
+)
+
+type Repeat struct {
+	from  int64
+	to    int64
+	token token.Token
+	value []token.Token
+}
+
+func NewRepeat(tok token.Token, from, to int64) *Repeat {
+	re := &Repeat{
+		from:  from,
+		to:    to,
+		token: tok,
+		value: make([]token.Token, from),
+	}
+
+	for i, _ := range re.value {
+		re.value[i] = tok.Clone()
+	}
+
+	return re
+}
+
+func (re *Repeat) Clone() token.Token {
+	c := Repeat{
+		from:  re.from,
+		to:    re.to,
+		token: re.token,
+		value: make([]token.Token, len(re.value)),
+	}
+
+	for i, tok := range re.value {
+		c.value[i] = tok.Clone()
+	}
+
+	return &c
+}
+
+func (re *Repeat) Fuzz(r rand.Rand) {
+	n := r.Intn(int(re.to-re.from+1)) + int(re.from)
+	toks := make([]token.Token, n)
+
+	for i, _ := range toks {
+		toks[i] = re.token.Clone()
+
+		toks[i].Fuzz(r)
+	}
+
+	re.value = toks
+}
+
+func (re *Repeat) String() string {
+	var buffer bytes.Buffer
+
+	for _, tok := range re.value {
+		buffer.WriteString(tok.String())
+	}
+
+	return buffer.String()
+}
---
+++ token/lists/repeat_test.go
@@ -0,0 +1,41 @@
+package logicals
+
+import (
+	"testing"
+
+	. "github.com/stretchr/testify/assert"
+
+	"github.com/zimmski/tavor/test"
+	"github.com/zimmski/tavor/token"
+	"github.com/zimmski/tavor/token/primitives"
+)
+
+func TestRepeatTokensToBeTokens(t *testing.T) {
+	var tok *token.Token
+
+	a := primitives.NewRandomInt()
+
+	Implements(t, tok, NewRepeat(a, 1, 5))
+}
+
+func TestRepeat(t *testing.T) {
+	a := primitives.NewConstantString("a")
+
+	o := NewRepeat(a, 5, 10)
+	Equal(t, "aaaaa", o.String())
+
+	r := test.NewRandTest(1)
+	o.Fuzz(r)
+	Equal(t, "aaaaaaa", o.String())
+
+	b := primitives.NewRangeInt(1, 3)
+	o = NewRepeat(b, 2, 10)
+	Equal(t, "11", o.String())
+
+	r.Seed(2)
+	o.Fuzz(r)
+	Equal(t, "12312", o.String())
+
+	o2 := o.Clone()
+	Equal(t, o.String(), o2.String())
+}
