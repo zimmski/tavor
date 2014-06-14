510789b6022b5cb9bc82ef01ea63996ac209d134
Markus Zimmermann <markus.zimmermann@nethead.at>
2014-06-14 15:33:30 +0200

    Fuzzing strategy to permute all optionals

---
+++ fuzz/strategy/permuteoptionals.go
@@ -0,0 +1,192 @@
+package strategy
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/zimmski/container/list/linkedlist"
+
+	"github.com/zimmski/tavor"
+	"github.com/zimmski/tavor/rand"
+	"github.com/zimmski/tavor/token"
+	"github.com/zimmski/tavor/token/lists"
+)
+
+type optionalLookup struct {
+	token  token.OptionalToken
+	childs []optionalLookup
+}
+
+type PermuteOptionalsStrategy struct {
+	root token.Token
+
+	continueFuzzing chan struct{}
+}
+
+func NewPermuteOptionalsStrategy(tok token.Token) *PermuteOptionalsStrategy {
+	s := &PermuteOptionalsStrategy{
+		root: tok,
+	}
+
+	return s
+}
+
+func init() {
+	Register("PermuteOptionals", func(tok token.Token) Strategy {
+		return NewPermuteOptionalsStrategy(tok)
+	})
+}
+
+func (s *PermuteOptionalsStrategy) findOptionals(root token.Token, fromChilds bool) []optionalLookup {
+	var optionals []optionalLookup
+	var queue = linkedlist.New()
+
+	if fromChilds {
+		switch t := root.(type) {
+		case token.ForwardToken:
+			queue.Push(t.Get())
+		case lists.List:
+			l := t.Len()
+
+			for i := 0; i < l; i++ {
+				c, _ := t.Get(i)
+				queue.Push(c)
+			}
+		}
+	} else {
+		queue.Push(root)
+	}
+
+	for !queue.Empty() {
+		tok, _ := queue.Shift()
+
+		switch t := tok.(type) {
+		case token.OptionalToken:
+			if tavor.DEBUG {
+				fmt.Printf("Found optional %#v\n", t)
+			}
+
+			t.Deactivate()
+
+			optionals = append(optionals, optionalLookup{
+				token:  t,
+				childs: nil,
+			})
+		case token.ForwardToken:
+			queue.Push(t.Get())
+		case lists.List:
+			l := t.Len()
+
+			for i := 0; i < l; i++ {
+				c, _ := t.Get(i)
+				queue.Push(c)
+			}
+		}
+	}
+
+	return optionals
+}
+
+func (s *PermuteOptionalsStrategy) Fuzz(r rand.Rand) chan struct{} {
+	continueFuzzing := make(chan struct{})
+
+	go func() {
+		if tavor.DEBUG {
+			fmt.Println("Start permute optionals routine")
+		}
+
+		optionals := s.findOptionals(s.root, false)
+
+		if len(optionals) != 0 {
+			if !s.fuzz(continueFuzzing, optionals) {
+				return
+			}
+		}
+
+		if tavor.DEBUG {
+			fmt.Println("Done with fuzzing step")
+		}
+
+		// done with the last fuzzing step
+		continueFuzzing <- struct{}{}
+
+		if tavor.DEBUG {
+			fmt.Println("Finished fuzzing. Wait till the outside is ready to close.")
+		}
+
+		if _, ok := <-continueFuzzing; ok {
+			if tavor.DEBUG {
+				fmt.Println("Close fuzzing channel")
+			}
+
+			close(continueFuzzing)
+		}
+	}()
+
+	return continueFuzzing
+}
+
+func (s *PermuteOptionalsStrategy) fuzz(continueFuzzing chan struct{}, optionals []optionalLookup) bool {
+	if tavor.DEBUG {
+		fmt.Printf("Fuzzing optionals %#v\n", optionals)
+	}
+
+	// TODO make this WAYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY smarter
+	// since we can only fuzz 64 optionals at max
+	p := 0
+	maxSteps := int(math.Pow(2, float64(len(optionals))))
+
+	for {
+		if tavor.DEBUG {
+			fmt.Printf("Fuzzing step %b\n", p)
+		}
+
+		ith := 1
+
+		for i := range optionals {
+			if p&ith == 0 {
+				optionals[i].token.Deactivate()
+				optionals[i].childs = nil
+			} else {
+				optionals[i].token.Activate()
+				optionals[i].childs = s.findOptionals(optionals[i].token, true)
+
+				if len(optionals[i].childs) != 0 {
+					if !s.fuzz(continueFuzzing, optionals[i].childs) {
+						return false
+					}
+				}
+			}
+
+			ith = ith << 1
+		}
+
+		p++
+
+		if p == maxSteps {
+			if tavor.DEBUG {
+				fmt.Println("Done with fuzzing these optionals")
+			}
+
+			break
+		}
+
+		if tavor.DEBUG {
+			fmt.Println("Done with fuzzing step")
+		}
+
+		// done with this fuzzing step
+		continueFuzzing <- struct{}{}
+
+		// wait until we are allowed to continue
+		if _, ok := <-continueFuzzing; !ok {
+			if tavor.DEBUG {
+				fmt.Println("Fuzzing channel closed from outside")
+			}
+
+			return false
+		}
+	}
+
+	return true
+}
---
+++ fuzz/strategy/permuteoptionals_test.go
@@ -0,0 +1,200 @@
+package strategy
+
+import (
+	"testing"
+
+	. "github.com/stretchr/testify/assert"
+
+	"github.com/zimmski/tavor/test"
+	"github.com/zimmski/tavor/token"
+	"github.com/zimmski/tavor/token/constraints"
+	"github.com/zimmski/tavor/token/lists"
+	"github.com/zimmski/tavor/token/primitives"
+)
+
+func TestPermuteOptionalsStrategyToBeStrategy(t *testing.T) {
+	var strat *Strategy
+
+	Implements(t, strat, &PermuteOptionalsStrategy{})
+}
+
+func TestPermuteOptionalsfindOptionals(t *testing.T) {
+	o := NewPermuteOptionalsStrategy(nil)
+
+	{
+		a := primitives.NewConstantInt(1)
+		b := constraints.NewOptional(primitives.NewConstantInt(2))
+		c := primitives.NewPointer(primitives.NewConstantInt(3))
+		d := lists.NewAll(a, b, c)
+
+		optionals := o.findOptionals(d, false)
+
+		Equal(t, optionals, []optionalLookup{
+			optionalLookup{
+				token:  b,
+				childs: nil,
+			},
+		})
+	}
+	{
+		a := constraints.NewOptional(primitives.NewConstantInt(1))
+		b := constraints.NewOptional(primitives.NewConstantInt(2))
+		c := lists.NewAll(a, b)
+		d := constraints.NewOptional(c)
+
+		optionals := o.findOptionals(d, false)
+
+		Equal(t, optionals, []optionalLookup{
+			optionalLookup{
+				token:  d,
+				childs: nil,
+			},
+		})
+
+		for i := range optionals {
+			optionals[i].token.(token.OptionalToken).Activate()
+			optionals[i].childs = o.findOptionals(optionals[i].token, true)
+		}
+
+		Equal(t, optionals, []optionalLookup{
+			optionalLookup{
+				token: d,
+				childs: []optionalLookup{
+					optionalLookup{
+						token:  a,
+						childs: nil,
+					},
+					optionalLookup{
+						token:  b,
+						childs: nil,
+					},
+				},
+			},
+		})
+	}
+	// TODO test list optional
+}
+
+func TestPermuteOptionalsStrategy(t *testing.T) {
+	// this random generator will never be touched since
+	// this strategy is not bound to randomness
+	r := test.NewRandTest(1)
+
+	{
+		a := constraints.NewOptional(primitives.NewConstantInt(1))
+		b := primitives.NewConstantInt(2)
+		c := constraints.NewOptional(primitives.NewConstantInt(3))
+		d := lists.NewAll(a, b, c)
+
+		o := NewPermuteOptionalsStrategy(d)
+
+		ch := o.Fuzz(r)
+
+		_, ok := <-ch
+		True(t, ok)
+		Equal(t, "2", d.String())
+		ch <- struct{}{}
+
+		_, ok = <-ch
+		True(t, ok)
+		Equal(t, "12", d.String())
+		ch <- struct{}{}
+
+		_, ok = <-ch
+		True(t, ok)
+		Equal(t, "23", d.String())
+		ch <- struct{}{}
+
+		_, ok = <-ch
+		True(t, ok)
+		Equal(t, "123", d.String())
+		ch <- struct{}{}
+
+		_, ok = <-ch
+		False(t, ok)
+
+		// rerun
+		ch = o.Fuzz(r)
+
+		_, ok = <-ch
+		True(t, ok)
+		Equal(t, "2", d.String())
+
+		close(ch)
+
+		// run with range
+		var got []string
+
+		ch = o.Fuzz(r)
+		for i := range ch {
+			got = append(got, d.String())
+
+			ch <- i
+		}
+
+		Equal(t, got, []string{
+			"2",
+			"12",
+			"23",
+			"123",
+		})
+	}
+	{
+		a := constraints.NewOptional(primitives.NewConstantInt(1))
+		b := constraints.NewOptional(primitives.NewConstantInt(2))
+		c := lists.NewAll(a, b)
+		d := constraints.NewOptional(c)
+
+		o := NewPermuteOptionalsStrategy(d)
+
+		var got []string
+
+		ch := o.Fuzz(r)
+		for i := range ch {
+			got = append(got, d.String())
+
+			ch <- i
+		}
+
+		Equal(t, got, []string{
+			"",
+			"",
+			"1",
+			"2",
+			"12",
+		})
+	}
+	{
+		a1 := constraints.NewOptional(primitives.NewConstantInt(1))
+		a2 := constraints.NewOptional(primitives.NewConstantInt(11))
+		a := constraints.NewOptional(lists.NewAll(a1, a2, primitives.NewConstantString("a")))
+		b := constraints.NewOptional(primitives.NewConstantString("b"))
+		c := lists.NewAll(a, b, primitives.NewConstantString("c"))
+		d := constraints.NewOptional(c)
+
+		o := NewPermuteOptionalsStrategy(d)
+
+		var got []string
+
+		ch := o.Fuzz(r)
+		for i := range ch {
+			got = append(got, d.String())
+
+			ch <- i
+		}
+
+		Equal(t, got, []string{
+			"",
+			"c",
+			"ac",
+			"1ac",
+			"11ac",
+			"111ac",
+			"bc",
+			"abc",
+			"1abc",
+			"11abc",
+			"111abc",
+		})
+	}
+}
