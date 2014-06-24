a21664337339bc1ef061d5ea24c96cba39331c27
Markus Zimmermann <markus.zimmermann@nethead.at>
2014-06-24 23:36:12 +0200

    rename tok->token and allow token to be nil

--- token/primitives/pointer.go
+++ token/primitives/pointer.go
@@ -9,38 +9,38 @@ import (
 )
 
 type Pointer struct {
-	tok token.Token
-	typ reflect.Type
+	token token.Token
+	typ   reflect.Type
 
 	cloned bool
 }
 
 func NewPointer(tok token.Token) *Pointer {
 	return &Pointer{
-		tok: tok,
-		typ: reflect.TypeOf(tok).Elem(),
+		token: tok,
+		typ:   reflect.TypeOf(tok).Elem(),
 	}
 }
 
 func NewEmptyPointer(typ interface{}) *Pointer {
 	return &Pointer{
-		tok: nil,
-		typ: reflect.TypeOf(typ).Elem(),
+		token: nil,
+		typ:   reflect.TypeOf(typ).Elem(),
 	}
 }
 
 func (p *Pointer) Clone() token.Token {
 	return &Pointer{
-		tok:    p.tok, // do not clone further
+		token:  p.token, // do not clone further
 		typ:    p.typ,
 		cloned: false,
 	}
 }
 
 func (p *Pointer) cloneOnFirstUse() {
-	if !p.cloned {
+	if !p.cloned && p.token != nil {
 		// clone everything on first use until we hit pointers
-		p.tok = p.tok.Clone()
+		p.token = p.token.Clone()
 
 		p.cloned = true
 	}
@@ -53,12 +53,16 @@ func (p *Pointer) Fuzz(r rand.Rand) {
 func (p *Pointer) FuzzAll(r rand.Rand) {
 	p.Fuzz(r)
 
+	if p.token == nil {
+		return
+	}
+
 	// fuzz with the clone not the original token
-	p.tok.FuzzAll(r)
+	p.token.FuzzAll(r)
 }
 
 func (p *Pointer) Get() token.Token {
-	return p.tok
+	return p.token
 }
 
 func (p *Pointer) Permutation(i int) error {
@@ -86,19 +90,33 @@ func (p *Pointer) Permutations() int {
 func (p *Pointer) PermutationsAll() int {
 	p.cloneOnFirstUse()
 
-	return p.tok.PermutationsAll()
+	if p.token == nil {
+		return 1
+	}
+
+	return p.token.PermutationsAll()
 }
 
 func (p *Pointer) Set(o token.Token) error {
+	if o == nil {
+		p.token = nil
+		p.cloned = true
+		return nil
+	}
+
 	if !reflect.TypeOf(o).Implements(p.typ) {
 		return fmt.Errorf("does not implement type %s", p.typ)
 	}
 
-	p.tok = o
+	p.token = o
 
 	return nil
 }
 
 func (p *Pointer) String() string {
-	return p.tok.String()
+	if p.token == nil {
+		return ""
+	}
+
+	return p.token.String()
 }
