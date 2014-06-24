f84c286f2dc5d7d7808baa2863cce1efd66df1c6
Markus Zimmermann <markus.zimmermann@nethead.at>
2014-06-24 23:29:42 +0200

    helper function PrettyPrintTree

--- tavor.go
+++ tavor.go
@@ -1,8 +1,38 @@
 package tavor
 
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/zimmski/tavor/token"
+	"github.com/zimmski/tavor/token/lists"
+)
+
 const (
 	Version = "0.1"
 )
 
 //TODO remove this
 var DEBUG = false
+
+func PrettyPrintTree(w io.Writer, root token.Token) {
+	prettyPrintTreeRek(w, root, 0)
+}
+
+func prettyPrintTreeRek(w io.Writer, tok token.Token, level int) {
+	fmt.Fprintf(w, "%s(%p)%#v\n", strings.Repeat("\t", level), tok, tok)
+
+	switch t := tok.(type) {
+	case token.ForwardToken:
+		if v := t.Get(); v != nil {
+			prettyPrintTreeRek(w, v, level+1)
+		}
+	case lists.List:
+		for i := 0; i < t.Len(); i++ {
+			c, _ := t.Get(i)
+
+			prettyPrintTreeRek(w, c, level+1)
+		}
+	}
+}
