67b7247593ba2182af96535600cf27d1dfe2d51d
Markus Zimmermann <markus.zimmermann@nethead.at>
2014-07-04 01:11:03 +0200

    pretty much redo the binary

--- bin/tavor.go
+++ bin/tavor.go
@@ -19,23 +19,39 @@ const (
 	returnOk = iota
 	returnHelp
 	returnBashCompletion
+	returnInvalidInputFile
 )
 
 var opts struct {
-	Config         func(s flags.Filename) error `long:"config" description:"INI config file" no-ini:"true"`
-	ConfigWrite    flags.Filename               `long:"config-write" description:"Write all arguments to an INI config file or to STDOUT with \"-\" as argument." no-ini:"true"`
-	Debug          bool                         `long:"debug" description:"Temporary debugging argument"`
-	FormatFile     flags.Filename               `long:"format-file" description:"Input tavor format file" required:"true" no-ini:"true"`
-	Help           bool                         `long:"help" description:"Show this help message" no-ini:"true"`
-	ListStrategies bool                         `long:"list-strategies" description:"List all available strategies." no-ini:"true"`
-	PrintInternal  bool                         `long:"print-internal" description:"Prints the internal AST of the parsed file"`
-	Seed           int64                        `long:"seed" description:"Seed for all the randomness"`
-	Strategy       Strategy                     `long:"strategy" description:"The fuzzing strategy" default:"random"`
-	Validate       bool                         `long:"validate" description:"Just validates the input file"`
-	Verbose        bool                         `long:"verbose" description:"Do verbose output."`
-	Version        bool                         `long:"version" description:"Print the version of this program." no-ini:"true"`
+	General struct {
+		Debug   bool `long:"debug" description:"Debug log output"`
+		Help    bool `long:"help" description:"Show this help message"`
+		Verbose bool `long:"verbose" description:"Verbose log output"`
+		Version bool `long:"version" description:"Print the version of this program"`
+	} `group:"General options"`
 
-	configFile string
+	Global struct {
+		Seed int64 `long:"seed" description:"Seed for all the randomness"`
+	} `group:"Global options"`
+
+	Format struct {
+		FormatFile    flags.Filename `long:"format-file" description:"Input tavor format file" required:"true"`
+		PrintInternal bool           `long:"print-internal" description:"Prints the internal AST of the parsed format file"`
+		Validate      bool           `long:"validate" description:"Just validate the format file and exit"`
+	} `group:"Format file options"`
+
+	Fuzz struct {
+		Strategy       Strategy `long:"strategy" description:"The fuzzing strategy" default:"random"`
+		ListStrategies bool     `long:"list-strategies" description:"List all available strategies"`
+	} `command:"fuzz"`
+
+	Reduce struct {
+		InputFile flags.Filename `long:"input-file" description:"Input file which gets parsed, validated and delta-debugged via the format file" required:"true"`
+	} `command:"reduce"`
+
+	Validate struct {
+		InputFile flags.Filename `long:"input-file" description:"Input file which gets parsed and validated via the format file" required:"true"`
+	} `command:"validate"`
 }
 
 type Strategy string
@@ -54,31 +70,25 @@ func (s *Strategy) Complete(match string) []flags.Completion {
 	return items
 }
 
-func checkArguments() {
+func checkArguments() string {
 	p := flags.NewNamedParser("tavor", flags.PassDoubleDash)
 
 	p.ShortDescription = "A fuzzing and delta-debugging platform."
 
-	opts.Config = func(s flags.Filename) error {
-		ini := flags.NewIniParser(p)
-
-		opts.configFile = string(s)
-
-		return ini.ParseFile(string(s))
+	if _, err := p.AddGroup("Tavor", "Tavor arguments", &opts); err != nil {
+		panic(err)
 	}
 
-	p.AddGroup("Tavor", "Tavor arguments", &opts)
-
 	_, err := p.Parse()
-	if opts.Help || len(os.Args) == 1 {
+	if opts.General.Help || len(os.Args) == 1 {
 		p.WriteHelp(os.Stdout)
 
 		os.Exit(returnHelp)
-	} else if opts.Version {
+	} else if opts.General.Version {
 		fmt.Printf("Tavor v%s\n", tavor.Version)
 
 		os.Exit(returnOk)
-	} else if opts.ListStrategies {
+	} else if opts.Fuzz.ListStrategies {
 		for _, name := range strategy.List() {
 			fmt.Println(name)
 		}
@@ -94,41 +104,31 @@ func checkArguments() {
 		os.Exit(returnBashCompletion)
 	}
 
-	if opts.ConfigWrite != "" {
-		ini := flags.NewIniParser(p)
-
-		var iniOptions flags.IniOptions = flags.IniIncludeComments | flags.IniIncludeDefaults | flags.IniCommentDefaults
-
-		if opts.ConfigWrite == "-" {
-			(ini.Write(os.Stdout, iniOptions))
-		} else {
-			ini.WriteFile(string(opts.ConfigWrite), iniOptions)
-		}
-
-		os.Exit(returnOk)
-	}
-
-	if opts.Seed == 0 {
-		opts.Seed = time.Now().UTC().UnixNano()
-	}
-
-	if opts.Debug {
+	if opts.General.Debug {
 		log.LevelDebug()
-	} else if opts.Verbose {
+	} else if opts.General.Verbose {
 		log.LevelInfo()
 	} else {
 		log.LevelWarn()
 	}
+
+	if opts.Global.Seed == 0 {
+		opts.Global.Seed = time.Now().UTC().UnixNano()
+	}
+
+	log.Infof("Using seed %d", opts.Global.Seed)
+
+	return p.Active.Name
 }
 
 func main() {
-	checkArguments()
+	command := checkArguments()
 
-	log.Infof("Open file %s", opts.FormatFile)
+	log.Infof("Open file %s", opts.Format.FormatFile)
 
-	file, err := os.Open(string(opts.FormatFile))
+	file, err := os.Open(string(opts.Format.FormatFile))
 	if err != nil {
-		panic(fmt.Errorf("cannot open tavor file %s: %v", opts.FormatFile, err))
+		panic(fmt.Errorf("cannot open tavor file %s: %v", opts.Format.FormatFile, err))
 	}
 	defer file.Close()
 
@@ -137,50 +137,86 @@ func main() {
 		panic(fmt.Errorf("cannot parse tavor file: %v", err))
 	}
 
-	log.Info("File is valid")
+	log.Info("Format file is valid")
 
-	if opts.PrintInternal {
+	if opts.Format.PrintInternal {
 		tavor.PrettyPrintInternalTree(os.Stdout, doc)
 	}
 
-	if opts.Validate {
+	if opts.Format.Validate {
 		os.Exit(returnOk)
 	}
 
-	log.Infof("Using seed %d", opts.Seed)
-	log.Infof("Counted %d overall permutations", doc.PermutationsAll())
+	r := rand.New(rand.NewSource(opts.Global.Seed))
 
-	r := rand.New(rand.NewSource(opts.Seed))
+	switch command {
+	case "fuzz":
+		log.Infof("Counted %d overall permutations", doc.PermutationsAll())
 
-	strat, err := strategy.New(string(opts.Strategy), doc)
-	if err != nil {
-		panic(err)
-	}
+		strat, err := strategy.New(string(opts.Fuzz.Strategy), doc)
+		if err != nil {
+			panic(err)
+		}
 
-	log.Infof("Using %s strategy", opts.Strategy)
+		log.Infof("Using %s strategy", opts.Fuzz.Strategy)
 
-	ch, err := strat.Fuzz(r)
-	if err != nil {
-		panic(err)
-	}
+		ch, err := strat.Fuzz(r)
+		if err != nil {
+			panic(err)
+		}
 
-	another := false
-	for i := range ch {
-		if !opts.Debug {
-			if another {
-				fmt.Println()
-			} else {
-				another = true
+		another := false
+		for i := range ch {
+			if !opts.General.Debug {
+				if another {
+					fmt.Println()
+				} else {
+					another = true
+				}
 			}
+
+			log.Debug("Result:")
+
+			fmt.Print(doc.String())
+			if opts.General.Debug {
+				fmt.Println()
+			}
+
+			ch <- i
+		}
+	case "reduce", "validate":
+		inputFile := opts.Validate.InputFile
+
+		if command == "reduce" {
+			inputFile = opts.Reduce.InputFile
 		}
 
-		log.Debug("Result:")
+		input, err := os.Open(string(inputFile))
+		if err != nil {
+			panic(fmt.Errorf("cannot open input file %s: %v", inputFile, err))
+		}
+		defer input.Close()
 
-		fmt.Print(doc.String())
-		if opts.Debug {
-			fmt.Println()
+		errs := parser.ParseInternal(doc, input)
+
+		if len(errs) == 0 {
+			log.Info("Input file is valid")
+		} else {
+			log.Info("Input file is invalid")
+
+			for _, err := range errs {
+				log.Error(err)
+			}
+
+			os.Exit(returnInvalidInputFile)
 		}
 
-		ch <- i
+		if command == "reduce" {
+			panic("TODO not implemented yet")
+		}
+	default:
+		log.Panicf("Unknown command %q", command)
 	}
+
+	os.Exit(returnOk)
 }
