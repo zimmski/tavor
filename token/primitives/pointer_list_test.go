8547014ddebb55642cd15d0156ce3e4b14c054b8
Markus Zimmermann <markus.zimmermann@nethead.at>
2014-08-13 23:41:51 +0200

    allow attributes to be used in tavor repeats

---
+++ token/primitives/pointer_list_test.go
@@ -0,0 +1,40 @@
+package primitives_test
+
+import (
+	"testing"
+
+	. "github.com/stretchr/testify/assert"
+
+	"github.com/zimmski/tavor/token"
+	"github.com/zimmski/tavor/token/lists"
+	"github.com/zimmski/tavor/token/primitives"
+)
+
+func TestPointerList(t *testing.T) {
+	a := primitives.NewRangeInt(4, 10)
+
+	// empty pointers should have a nil token
+	{
+		var tok *token.Token
+
+		o := primitives.NewEmptyPointer(tok)
+		Nil(t, o.Get())
+
+		err := o.Set(a)
+		Nil(t, err)
+		Equal(t, a, o.Get())
+
+		var list *lists.List
+		o = primitives.NewEmptyPointer(list)
+
+		err = o.Set(a)
+		NotNil(t, err)
+		Nil(t, o.Get())
+
+		l := lists.NewAll(a)
+
+		err = o.Set(l)
+		Nil(t, err)
+		Equal(t, l, o.Get())
+	}
+}
